57bb1fbf3349a753e995b20d0e93ce44d8f1d058
mahikero <federicoparassarmiento@yahoo.com>
2016-05-05 04:58:12 +0800

    up

--- ACME/Models/Product.cs
+++ ACME/Models/Product.cs
@@ -11,10 +11,12 @@ namespace ACME.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Product
     {
         public System.Guid ProductId { get; set; }
+        [Display(Name="Product Name")]
         public string Name { get; set; }
         public decimal Price { get; set; }
     }
